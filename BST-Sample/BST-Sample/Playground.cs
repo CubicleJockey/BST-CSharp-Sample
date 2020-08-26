005ea889975c3aa84df998a3f7b24871956a1698
André Davis <CubicleJockey@users.noreply.github.com>
2020-08-25 22:36:54 -0700

    Prepping for BST example

---
+++ BST-Sample/BST-Sample/Playground.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BST_Sample
+{
+    [TestClass]
+    public class Playground
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
